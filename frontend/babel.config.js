c40f3d461acac567662145b1b28576b5bd7f646d
suheda-snr <t3sesu00@students.oamk.fi>
2025-11-10 11:23:27 +0200

    Update expo and some dependencies to fix uncompability errors

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,11 +3,11 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "safe": false,
-        "allowUndefined": true
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true
       }]
     ]
   };
