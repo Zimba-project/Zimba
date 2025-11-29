50563c042a8fb94ec7626132c3d8d6f261188e54
Ville-A <ville.m.ahola@gmail.com>
2025-11-29 02:41:49 +0200

    latest-ville.4.forgotBackend

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 module.exports = app;
